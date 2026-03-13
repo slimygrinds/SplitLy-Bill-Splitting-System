d0f3f7154cca3e889fec34d8537a31a13e693a3c
slimygrinds <aum.vachhani129527@marwadiuniversity.ac.in>
2026-03-13 14:25:00 +0530

    Created a CreateBill function and added its UI

---
+++ CreateBill.cs
@@ -0,0 +1,8 @@
+private void CreateBill()
+{
+    participants.Clear();
+    Console.Clear();
+
+    Console.WriteLine("=================================================");
+    Console.WriteLine("                CREATE NEW BILL");
+    Console.WriteLine("=================================================\n");
