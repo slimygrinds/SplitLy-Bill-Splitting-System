d8c3c0d04a557434fe033ce5e79005bb4d3e28a5
slimygrinds <aum.vachhani129527@marwadiuniversity.ac.in>
2026-03-17 14:12:00 +0530

    Initialized EnterParticipants UI section

---
+++ EnterParticipants.cs
@@ -0,0 +1,9 @@
+private void EnterParticipants(int count)
+    {
+        Console.Clear();
+
+        Console.WriteLine("=================================================");
+        Console.WriteLine("              PARTICIPANT DETAILS");
+        Console.WriteLine("=================================================\n");
+
+    }
